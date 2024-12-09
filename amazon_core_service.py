b7888bd07a554c9904766d00e578348ce0103c80
Sree Aditya <165385350+SreeAditya-Dev@users.noreply.github.com>
2024-12-09 13:48:29 +0530

    Update amazon_core_service.py

--- amazon_core_service.py
+++ amazon_core_service.py
@@ -1021,8 +1021,7 @@ def secret_amazon_service():
 # Hash 997
 # Hash 998
 # Hash 999
-# Hash 1000
-# Hash 1001 - The Legend of the Missing Hash
+# Hash 1000 - The Legend of the Missing Hash
     return "Everything is technically working as intended... technically"
 
 # the last one!!!!!!!
